4d45288b9aa725c3a3d81bf1d85bff506182b615
= <=>
2026-01-20 08:34:58 +0200

    renamed the api again

--- src/hooks/useFetchLocation.js
+++ src/hooks/useFetchLocation.js
@@ -7,7 +7,7 @@ export function useFetchLocation(longitude, latitude) {
   const [locationLoading, setLocationLoading] = useState(false);
   const [error, setError] = useState();
 
-  const apiKey = import.meta.env.REVERSE_GEOLOCATION;
+  const apiKey = import.meta.env.VITE_REVERSE_GEOLOCATION;
 
   useEffect(() => {
     async function fetchLocationData() {
